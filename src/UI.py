203a0a5688a8df0a1155ab9ed702db5a65e90e0c
surtux <surtux@users.noreply.github.com>
2023-01-30 11:53:40 +0100

    Update UI.py
    Adding code in UI file

--- src/UI.py
+++ src/UI.py
@@ -1,1 +1,14 @@
+"""
+This is the UI code. One thing, actually I don't now
+how to test the elements of this UI.
+"""
+import PySimpleGUI as sg
 
+#I declare the layout here
+layout = [
+  [sg.Text('Weight'), [sg.Input()]]
+  [sg.Text('height'), [sg.input()]],
+  [sg.Text('BMI'), [sg.text('N/A')]]
+]
+
+windows = sg.Windows('Calculate your BMI', layout).read()
