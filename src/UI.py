36b3fdddb103b2efced360828835b5043ef3e6dc
surtux <surtux@users.noreply.github.com>
2023-01-30 16:33:43 +0100

    Update UI.py
    Ajusting line for Pylint

--- src/UI.py
+++ src/UI.py
@@ -8,13 +8,13 @@ from bmic.src import health
 #I declare the layout here
 layout = [
   [sg.Text('Weight'), [sg.Input(key = 'input1')]]
-  [sg.Text('height'), [sg.input(key = 'input2')]],
+  [sg.Text('height'), [sg.Input(key = 'input2')]],
   [sg.Button('Calculate', key = 'calculate')]
-  [sg.Text('BMI', enable_events = True), [sg.text('N/A', key = 'bmi')]]
+  [sg.Text('BMI', enable_events = True), [sg.Text('N/A', key = 'bmi')]]
 ]
 
 #The line below create the windows
-window = sg.Windows('Calculate your BMI', layout)
+window = sg.Window('Calculate your BMI', layout)
 
 #infinite loop to catch different events related to the application
 while True:
@@ -22,8 +22,7 @@ while True:
   event, values = window.read()
   #Line use to close the windows when the user hit the cross icon above the windows
   if event == sg.WIN_CLOSED:
-    break
-    
+    break  
   #From the line below, we get the value of the height and weight and compute the bmi
   if event == 'calculate':
     height = values['input2']
