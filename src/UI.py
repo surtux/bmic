0a0d0e274d4cb40a3089ad7086fabc627d47fd73
surtux <surtux@users.noreply.github.com>
2023-01-30 16:54:43 +0100

    Update UI.py
    Clean with Pycharm

--- src/UI.py
+++ src/UI.py
@@ -2,32 +2,34 @@
 This is the UI code. One thing, actually I don't now
 how to test the elements of this UI.
 """
+# pylint: disable=C0103
 import PySimpleGUI as sg
-import health
+import bmic.health
 
 #I declare the layout here
-layout = [
-  [sg.Text('Weight'), [sg.Input(key = 'input1')]]
-  [sg.Text('height'), [sg.Input(key = 'input2')]],
-  [sg.Button('Calculate', key = 'calculate')]
-  [sg.Text('BMI', enable_events = True), [sg.Text('N/A', key = 'bmi')]]
+# pylint: disable=C0330
+LAYOUT = [
+  [sg.Text('Weight'), [sg.Input(key='input1')]],
+  [sg.Text('height'), [sg.Input(key='input2')]],
+  [sg.Button('Calculate', key='calculate')],
+  [sg.Text('BMI', enable_events=True), [sg.Text('N/A', key='bmi')]]
 ]
 
 #The line below create the windows
-window = sg.Window('Calculate your BMI', layout)
+WINDOW = sg.Window('Calculate your BMI', LAYOUT)
 
 #infinite loop to catch different events related to the application
 while True:
-  #The line use below, allowed to catch windows events an their values
-  event, values = window.read()
-  #Line use to close the windows when the user hit the cross icon above the windows
-  if event == sg.WIN_CLOSED:
-    break  
-  #From the line below, we get the value of the height and weight and compute the bmi
-  if event == 'calculate':
-    height = values['input2']
-    weight = values['input1']
-    bmi = health.imc(height, weight)
-    window['bmi'].update(str(bmi))
+    #The line use below, allowed to catch windows events an their values
+    EVENT, VALUES = WINDOW.read()
+    #Line use to close the windows when the user hit the cross icon above the windows
+    if EVENT == sg.WIN_CLOSED:
+        break
+    #From the line below, we get the value of the height and weight and compute the bmi
+    if EVENT == 'calculate':
+        HEIGHT = VALUES['input2']
+        WEIGHT = VALUES['input1']
+        BMI = bmic.health.imc(HEIGHT, WEIGHT)
+        WINDOW['bmi'].update(str(BMI))
 #Just to be sure that the window will be close properly
-window.close()
+WINDOW.close()
