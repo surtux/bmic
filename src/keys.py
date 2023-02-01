fb978d00944fd0faf296e0fd94fd861a7c15b377
surtux <tsafackghokeng@gmail.com>
2023-02-01 05:26:07 +0100

    Adding url in keys.py

--- src/keys.py
+++ src/keys.py
@@ -1,2 +1,3 @@
 APIKEY = "wDJX7b-PUnvKiEBv7mM3gcKPSC6LiTSZGbMAGs_HKzDS"
 HOST = "201c1335-bf3b-4f8a-b123-0c3cea753ce6-bluemix.cloudantnosqldb.appdomain.cloud"
+URL = "https://apikey-v2-1d0pzh5us48t4npgz7wzon2rrm693ndf4lmedu8o8hpw:d9d095a9141c8725868705f82fb9a4c4@201c1335-bf3b-4f8a-b123-0c3cea753ce6-bluemix.cloudantnosqldb.appdomain.cloud"
